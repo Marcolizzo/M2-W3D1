276b87d1fd7d1fa4d515bd2ceab0064a1026311d
Marco Santoro <msantor@hotmail.it>
2023-10-26 18:54:37 +0200

    V2
    Inseriti esercizi extra e pulizia del codice

--- M2-W3D1.js
+++ M2-W3D1.js
@@ -42,7 +42,7 @@ const reverseString = function (stringa) {
     con ".reverse" ed infine ritorno l'array di lettere di nuovo come stringa con ".join" */
     return stringa.split("").reverse().join("");
 }
-console.log(reverseString("EPICODE"))
+console.log(reverseString("Questa frase deve essere scritta al contrario"))
 console.log("")
 
 /* ESERCIZIO 4
@@ -63,7 +63,7 @@ const upperFirst = function (stringa) {
     //ritorno l'array come stringa sempre usando lo spazio come separatore
     return splitStringa.join(" ")
 }
-console.log(upperFirst("ciao sono marco"))
+console.log(upperFirst("questa frase deve avere tutte le parole con l'iniziale maiuscola"))
 console.log("")
 
 /* ESERCIZIO 5
@@ -75,6 +75,7 @@ console.log("Esercizio 5")
 const giveMeRandom = function (n) {
     const numeri = []
     for (let i = 0; i < n; i++) {
+        // creo un loop che mi dia n numeri random che arrotonderò con Math.round e aggiungerò all'array "numeri" con .push
         numeri.push(Math.round(Math.random() * 11))
     }
     return numeri
@@ -89,6 +90,13 @@ console.log("")
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+console.log("Esercizio extra 1")
+const area = function (l1, l2) {
+    // ritorno la moltiplicazione
+    return l1 * l2
+}
+console.log(area(3, 4))
+console.log("")
 
 /* ESERCIZIO 2
  Scrivi una funzione chiamata "crazyDiff" che calcola la differenza assoluta tra un numero fornito e 19.
@@ -96,6 +104,20 @@ console.log("")
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+console.log("Esercizio extra 2")
+const crazyDiff = function (x) {
+    /*converto la sottrazione "19-x" in numero assoluto e controllo se controllo se è maggiore
+    di 19. Se lo è, faccio ritornare il risultato moltiplicato per 3, altrimenti 
+    faccio ritornare solo il risultato della sottrazione */
+    if (Math.abs(19 - x) > 19) {
+        return Math.abs((19 - x) * 3)
+    } else {
+        return Math.abs(19 - x)
+    }
+}
+console.log(crazyDiff(39))
+console.log("")
+
 
 /* ESERCIZIO 3
  Scrivi una funzione chiamata "codify" che accetta una stringa come parametro.
@@ -104,6 +126,19 @@ console.log("")
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+console.log("Esercizio extra 3")
+const codify = function (stringa) {
+    /* Controllo se nella stringa è contenuta la stringa "code" nella prima posizione (indexOf() ===0),
+    se lo è allora ritorno la stringa senza modifiche, altrimenti aggiungo la stringa "code " alla stringa*/
+    if (stringa.indexOf("code") === 0) {
+        return stringa
+    } else {
+        return "code " + stringa
+    }
+}
+console.log(codify('questa frase deve iniziare con "code"'))
+console.log("")
+
 /* ESERCIZIO 4
  Scrivi una funzione chiamata "check3and7" la quale accetta un numero intero positivo come parametro.
  La funzione deve controllare che tale parametro sia un multiplo di 3 o di 7, e in tal caso tornare true; altrimenti deve tornare false.
@@ -111,9 +146,35 @@ console.log("")
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+console.log("Esercizio extra 3")
+const check3and7 = function (x) {
+    /*creo una variabile "numero" e converto x in un numero assoluto e lo arrotondo. */
+    let numero = parseInt(Math.round(Math.abs(x)))
+    /*Controlo se la variabile numero è un multiplo di 3 o di 7 con l'operatore modulo,
+    se lo è ritorno true, altrimenti ritorno false */
+    if ((numero % 3 === 0) || (numero % 7 === 0)) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(check3and7(-13.65))
+console.log("")
 
 /* ESERCIZIO 5
  Scrivi una funzione chiamata "cutString", che accetta una stringa come parametro e la ritorna senza il primo e l'ultimo carattere.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const cutString = function (stringa) {
+    /*creo una variabile per dividere la stringa in un array temporaneo con .split("") in modo
+    da dividere la parola/frase carattere per carattere */
+    let splitStringa = stringa.split("")
+    /*con .splice(0,1) elimino un carattere (1) nella prima posizione (0) */
+    splitStringa.splice(0, 1);
+    /*con .splice(-1,1) elimino un carattere (1) nell'ultima posizione (-1) */
+    splitStringa.splice(-1, 1);
+    /*infine riconverto l'array in stringa con .join("") e la ritorno */
+    return splitStringa.join("")
+}
+console.log(cutString("In questa frase verranno eliminati la prima e l'ultima lettera"))
